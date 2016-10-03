54d746091ff0848c77fbef518275351e196bc2e7
hiiq <highiq@email.com>
2016-10-03 01:33:02 -0400

    update to continuous integration configuration

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 'use strict';
 
 var express = require('express'),
-  app = express();
+  app = express(),
+  converter = require("./converter");;
 
 app.get('/', function (req, res) {
   res.status(200)
     .send('Success is going from failure to failure without losing enthusiasm.');
+});
+
+app.get("/rgbToHex", function(req, res) {
+  var red   = parseInt(req.query.red, 10);
+  var green = parseInt(req.query.green, 10);
+  var blue  = parseInt(req.query.blue, 10);
+  var hex = converter.rgbToHex(red, green, blue);
+  res.send(hex);
+});
+
+app.get("/hexToRgb", function(req, res) {
+  var hex = req.query.hex;
+  var rgb = converter.hexToRgb(hex);
+  res.send(JSON.stringify(rgb));
 });
 
 app.set('port', process.env.PORT || 8080);
